22810763e917c1618ae43d326cc7f0d21e52bb16
Svetlana Filimonova <sfilimonova@me.com>
2014-10-02 22:23:50 +1000

    Init project

---
+++ src/test/scala/com/whc/calculator/CalculatorSpec.scala
@@ -0,0 +1,13 @@
+package com.whc.calculator
+
+import org.scalatest.FunSpec
+
+class CalculatorSpec extends FunSpec {
+
+  describe("A Calculator") {
+
+    it("should be truthy") {
+      assert(true === true)
+    }
+  }
+}
