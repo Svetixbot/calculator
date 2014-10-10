d65b34a601209344273b9cf5c9b37610dadbb54a
Svetlana Filimonova <sfilimonova@me.com>
2014-10-10 17:19:05 +1100

    Remove a solution

--- src/main/scala/com/whc/calculator/Calculator.scala
+++ src/main/scala/com/whc/calculator/Calculator.scala
@@ -1,46 +1,7 @@
 package com.whc.calculator
 
 object Calculator {
-
-  sealed trait Operation
-
-  case object Plus extends Operation
-
-  case object Minus extends Operation
-
-  case object Multiply extends Operation
-
-  def operation(op: Char): Result[Operation] = op match {
-    case '+' => Ok(Plus)
-    case '-' => Ok(Minus)
-    case '*' => Ok(Multiply)
-    case _ => Fail(InvalidOperation("Invalid Operation"))
-  }
-
-  def calculate(op: Operation, n: Int, m: Int): Int = op match {
-    case Plus => n + m
-    case Minus => n - m
-    case Multiply => n * m
-  }
-
-  def attempt(op: Char, n: Int, m: Int): Result[Int] =
-    for {
-      op1 <- operation(op)
-    } yield calculate(op1, n, m)
-
-  def operationParser: Parser[(Char, Int, Int)] =
-    for {
-      n <- Parser.natural
-      _ <- Parser.list(Parser.space)
-      op <- Parser.character
-      _ <- Parser.list(Parser.space)
-      a <- Parser.natural
-    } yield new Tuple3(op, n, a)
-  
   def run(operation: String): Result[Int] = {
-      operationParser.run(operation) match {
-        case Ok(ParseState(_,res)) => attempt(res._1,res._2,res._3)
-        case Fail(error) => Fail(error)
-      }
+      return Ok(0);
   }
 }
--- src/main/scala/com/whc/calculator/Result.scala
+++ src/main/scala/com/whc/calculator/Result.scala
@@ -34,11 +34,7 @@ sealed trait Result[A] {
    * scala> Fail[Int](NotEnoughInput).fold(_ => 0, x => x)
    *  = 0
    */
-  def fold[X](fail: Error => X, ok: A => X): X = this match {
-    case Fail(error) => fail(error)
-    case Ok(a) => ok(a)
-  }
-
+  def fold[X](fail: Error => X, ok: A => X): X = ???
 
   /*
    * Exercise 2:
@@ -57,8 +53,7 @@ sealed trait Result[A] {
    *
    * Advanced: Try using flatMap.
    */
-  def map[B](f: A => B): Result[B] =
-    fold(Fail[B], a => Ok(f(a)))
+  def map[B](f: A => B): Result[B] = ???
 
   /*
    * Exercise 3:
@@ -82,6 +77,5 @@ sealed trait Result[A] {
    *
    * Advanced: Try using fold.
    */
-  def flatMap[B](f: A => Result[B]): Result[B] =
-    fold(Fail[B], f)
+  def flatMap[B](f: A => Result[B]): Result[B] = ???
 }
