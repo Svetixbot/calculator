d78e19bcecef000ea9c7dd10918b3474462a8ad0
Svetlana Filimonova <sfilimonova@me.com>
2014-11-02 11:04:45 +1100

    add ignore flag to each test in calc spec

--- src/test/scala/com/whc/calculator/CalculatorSpec.scala
+++ src/test/scala/com/whc/calculator/CalculatorSpec.scala
@@ -2,42 +2,41 @@ package com.whc.calculator
 
 import org.scalatest.{Ignore, FunSpec}
 
-@Ignore
 class CalculatorSpec extends FunSpec {
   describe("A Calculator") {
-    it("should add two numbers") {
+    ignore("should add two numbers") {
       assert(Calculator.run("1+2") === Ok(3))
     }
 
-    it("should subtract two numbers") {
+    ignore("should subtract two numbers") {
       assert(Calculator.run("3-2") === Ok(1))
     }
 
-    it("should multiply a number by another number") {
+    ignore("should multiply a number by another number") {
       assert(Calculator.run("3*2") === Ok(6))
     }
 
-    it("should fail if the input contains only one operand"){
+    ignore("should fail if the input contains only one operand"){
       assert(Calculator.run("3") === Fail(NotEnoughInput))
     }
 
-    it("should fail if we have only the first operand & operator"){
+    ignore("should fail if we have only the first operand & operator"){
       assert(Calculator.run("2+") === Fail(NotEnoughInput))
     }
 
-    it("should fail if one of the operands is not a number"){
+    ignore("should fail if one of the operands is not a number"){
       assert(Calculator.run("2+a") === Fail(UnexpectedInput("a")))
     }
 
-    it("should calculate if there are spaces"){
+    ignore("should calculate if there are spaces"){
       assert(Calculator.run("2 + 2") === Ok(4))
     }
 
-    it("should fail if there are spaces at the beginning and at the end"){
+    ignore("should fail if there are spaces at the beginning and at the end"){
       assert(Calculator.run(" 2 + 2 ") === Fail(UnexpectedInput(" 2 + 2 ")))
     }
 
-    it("should fail if the operator is not supported"){
+    ignore("should fail if the operator is not supported"){
       assert(Calculator.run("2%2") === Fail(InvalidOperation("%")))
     }
 
